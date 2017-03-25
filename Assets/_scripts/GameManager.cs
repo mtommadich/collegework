86abd6c1529bbd3d00cc824b0a0cd44bdbd7b724
mtommadich <x15007740@student.ncirl.ie>
2017-03-25 22:37:46 +0000

    Revert "commit from workstation"
    This reverts commit cad40e76998a8327262e5fe1e44f17f64f0e05a9.

--- Assets/_scripts/GameManager.cs
+++ Assets/_scripts/GameManager.cs
@@ -19,7 +19,6 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {		
-
 		unlockedScene = SceneManager.GetActiveScene().name;
 		maxLives = 8;
 		currLives = 8;
@@ -85,7 +84,6 @@ public class GameManager : MonoBehaviour {
 //	}
 
 	public void continueGame(){
-		loadingimage.SetActive (true);
 		SceneManager.LoadScene (unlockedScene);
 	}
 
@@ -117,7 +115,6 @@ public class GameManager : MonoBehaviour {
 	}
 
 	public void loadNextScene (){
-		loadingimage.SetActive (true);
 		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
 
 	}
